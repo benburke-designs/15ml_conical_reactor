5167e7c7d96f63c23d42b0870be1512a37303da0
benburke-designs <bburke7396@gmail.com>
2025-09-12 12:16:37 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/build_page",
+  base: "/15ml_conical_reactor",
 });
